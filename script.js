153424810cc6353d94c75d4142a63e6ac5d09d49
Melissa <matindimelissa@gmail.com>
2025-09-19 07:47:45 -0700

    Code inputs

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// Global variable
+let globalCount = 0;
 
+// Function with parameters & return value
+function calculateArea(width, height) {
+  return width * height;
+}
+
+// Function to trigger DOM change (animate box)
+function animateBox(id, duration = 600) {
+  const element = document.getElementById(id);
+  element.classList.add("animate");
+
+  // Remove the animation class after the given duration
+  setTimeout(() => element.classList.remove("animate"), duration);
+}
+
+// Demonstrating scope
+function incrementCounter() {
+  let localCount = 1; // local scope
+  globalCount += localCount;
+  return globalCount;
+}
